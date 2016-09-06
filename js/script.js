ee7e02f0dd37a1fd3b5e1066108cfca877b4bc7c
Irina <irstrashuk@gmail.com>
2016-09-06 09:39:59 +0300

    slider mistake

---
+++ js/script.js
@@ -0,0 +1,69 @@
+// function initialize() {
+//   var mapOptions = {
+//     zoom: 15,
+//     center: new google.maps.LatLng(59.93886, 30.32301)
+//   }
+//   var map = new google.maps.Map(document.getElementById('map-canvas'),
+//                                 mapOptions);
+//   var image = "http://i.neoseeker.com/mgv/499846/846/33/15847_179440563793_179437393793_2772323_4548598_n_icon.jpg";
+//   var myLatLng = new google.maps.LatLng(59.93886, 30.32301);
+//   var beachMarker = new google.maps.Marker({
+//     position: myLatLng,
+//     map: map,
+//     icon: image
+//   });
+// }
+// google.maps.event.addDomListener(window, 'load', initialize);
+
+var feedbackLink = document.querySelector(".mail-btn");
+var feedbackPopup = document.querySelector(".feedback");
+var feedbackClose = feedbackPopup.querySelector(".feedback__close");
+var feedbackForm = feedbackPopup.querySelector("form");
+
+var feedbackName = feedbackPopup.querySelector("[name=name]");
+var feedbackEmail = feedbackPopup.querySelector("[name=email]");
+
+
+feedbackLink.addEventListener("click", function(event) {
+  event.preventDefault();
+  feedbackPopup.classList.add("feedback-show");
+  feedbackName.focus();
+});
+
+feedbackClose.addEventListener("click", function(event) {
+  event.preventDefault();
+  feedbackPopup.classList.remove("feedback-show");
+});
+
+feedbackForm.addEventListener("submit", function(event) {
+  if (!feedbackName.value || !feedbackEmail.value){
+  event.preventDefault();
+  console.log("Поля имя и емейл обязательны для ввода!");
+ }
+
+});
+
+window.addEventListener("keydown", function(event) {
+  if (feedbackPopup.classList.contains("feedback-show")) {
+    feedbackPopup.classList.remove("feedback-show");
+  }
+});
+
+
+
+
+
+
+var link = document.querySelector(".map");
+    var popup = document.querySelector(".map-popup");
+    var close = popup.querySelector(".map-popup__close");
+
+    link.addEventListener("click", function(event) {
+      event.preventDefault();
+      popup.classList.add("map-popup-show");
+    });
+
+    close.addEventListener("click", function(event) {
+      event.preventDefault();
+      popup.classList.remove("map-popup-show");
+  });
